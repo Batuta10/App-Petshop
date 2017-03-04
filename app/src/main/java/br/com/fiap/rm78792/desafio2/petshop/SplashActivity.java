4d7fe6f208fce5562894320210b3d395788f4635
batuta2604@gmail.com <batuta2604@gmail.com>
2017-03-04 18:15:59 -0300

    Renaming activity

--- app/src/main/java/br/com/fiap/rm78792/desafio2/petshop/SplashActivity.java
+++ app/src/main/java/br/com/fiap/rm78792/desafio2/petshop/SplashActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Main2Activity extends AppCompatActivity implements Runnable {
+public class SplashActivity extends AppCompatActivity implements Runnable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
